9a61e13a92d0522db7efaa7266aa23b079678ff9
yuxizhou <yuxizhou@qingtingfm.com>
2014-11-07 16:19:34 +0800

    weixin reserve

--- handlers_mobile.py
+++ handlers_mobile.py
@@ -34,6 +34,6 @@ class ReserveMHandler(BaseHandler):
         rid = yield future
 
         if rid:
-            self.render('reserve_success.html')
+            self.render('/mobile/wx_reserve_success.html')
         else:
-            self.render('reserve_fail.html')
+            self.render('/mobile/wx_reserve_fail.html')
